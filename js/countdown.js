17b3fee4f3a2ad563736529fc8a70e5dfa79fe9f
Vini5610 <79315178+Vini5610@users.noreply.github.com>
2022-11-07 21:25:08 -0300

    Create countdown.js

---
+++ js/countdown.js
@@ -0,0 +1,33 @@
+var check = false
+function xequeMate(){
+	if (check == false){
+		var timer1 = setInterval(function(){start()}, 1000);
+		var timer2 = setTimeout(function(){end()}, 13200);
+		var count = 10;
+		function start(){
+			soundBeep();
+			document.getElementById("time").innerHTML = count;
+			if (count == 0){
+				clearInterval(timer1);			
+				soundThunder();
+				document.getElementById("fire").src = "dolar.gif";
+				document.getElementById("time").innerHTML ="GAME OVER";
+			}
+			count --;
+	}
+	check = true;
+	}
+}
+
+
+
+function soundThunder(){
+	var beep = new Audio();
+	beep.src = "Dolla.mp3";
+	beep.play();
+}
+function soundBeep(){
+	var beep = new Audio();
+	beep.src = "Beep_Shor.mp3";
+	beep.play();
+}
